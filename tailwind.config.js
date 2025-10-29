92bcbba00c0bc929e2df23082747d6b9717a4aa0
Jagadesh08Madhu <jagadesh2001kavi@gmail.com>
2025-10-29 21:36:41 +0530

    Logo

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,8 @@
+import { keyframes } from "framer-motion";
 const withMT = require("@material-tailwind/react/utils/withMT");
 
 /** @type {import('tailwindcss').Config} */
-export default {
+export default withMT({
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
@@ -11,27 +12,31 @@ export default {
       colors: {
         primary: 'rgb(3 7 18)',
       },
-      fontFamily:{
-        alice:["Alice" , "serif"],
-        lora:["Lora" , "serif"],
-        Sacramento:["Sacramento" , "serif"],
-        Inter:["Inter" , "sans-serif"],
-        Afacad:["Afacad Flux" , "sans-serif"],
-        // montserrat: ['Montserrat', 'sans-serif'],
-        syne: ['Syne', 'sans-serif'],
-        Afacad: ['Afacad Flux', 'sans-serif'],
-        
+      fontFamily: {
+        alice: ["Alice", "serif"],
+        lora: ["Lora", "serif"],
+        Sacramento: ["Sacramento", "serif"],
+        Inter: ["Inter", "sans-serif"],
+        Afacad: ["Afacad Flux", "sans-serif"],
+        syne: ["Syne", "sans-serif"],
       },
       animation: {
         'fade-in': 'fadeIn 0.8s ease-in forwards',
+        'loader': 'loader 1.5s ease-in-out infinite alternate',
       },
       keyframes: {
         fadeIn: {
           '0%': { opacity: 0 },
           '100%': { opacity: 1 },
         },
+        loader: {
+          '0%': { opacity: 0, transform: 'scale(0.9)' },   // ✅ string
+          '50%': { opacity: 1, transform: 'scale(1.05)' },  // ✅ string
+          '100%': { opacity: 1, transform: 'scale(1)' }     // ✅ string
+        },
+        
       },
     },
   },
   plugins: [],
-}
+});
