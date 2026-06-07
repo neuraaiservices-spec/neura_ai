73f75369f645c8e21ac919bdddc4bd17fe5ee54c
Yuvaraj <yuvaraj@Yuvarajs-MacBook-Air.local>
2026-06-07 18:19:57 +0530

    test

--- src/pages/KeyFocus.jsx
+++ src/pages/KeyFocus.jsx
@@ -43,7 +43,7 @@ export default function KeyFocus() {
         {
           img: dermatology,
           Imgicon: skin,
-          title: "Summa",
+          title: "Trichology",
           description: "AI-based skin anomaly classification and tracking."
         },
         {
